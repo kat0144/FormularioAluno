8b07f403a987c5063508826edb5a13f58994b0c0
kat0144 <khatlleen@gmail.com>
2025-11-04 00:54:59 -0300

    Adicionar funcoes novas

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//classe pessoa
 class Aluno {
     constructor(nome, idade, curso, notaFinal) {
         this.nome = nome;
@@ -6,6 +5,29 @@ class Aluno {
         this.curso = curso;
         this.notaFinal = notaFinal;
     }
+
+    isAprovado() {
+        if (this.notaFinal >= 7) {
+            return true; 
+        }
+
+        else {
+            return false;
+        }
+    }
+
+    toString() {
+        const status = this.isAprovado() ? "Aprovado" : "Reprovado";
+
+        return `--- Dados do Aluno ---
+        Nome: ${this.nome}
+        Idade: ${this.idade} anos
+        Curso: ${this.curso}
+        Nota Final: ${this.notaFinal.toFixed(1).replace('.', ',')}
+        Status: ${status}`;
+    }
+
+
 }
 
 console.log('Funcionando!');
@@ -17,55 +39,105 @@ class AlunoController {
     }
 
     //adiciona evento no button para cadastrar - arrow function
-    init(){
+    init() {
         document.getElementById('btnCadastrar').addEventListener('click', (e) => this.cadastrarAluno(e));
         document.getElementById('btnExcluir').addEventListener('click', (e) => this.excluirAluno(e));
         document.getElementById('btnEditar').addEventListener('click', (e) => this.editarAluno(e));
-    }   
+    }
 
 
     //cadastra um aluno na lista 
-    cadastrarAluno(e){
+    cadastrarAluno(e) {
         e.preventDefault();
         const nome = document.getElementById('nome').value;
-        const idade = document.getElementById('idade').value;
+        const idade = parseInt(document.getElementById('idade').value);
         const curso = document.getElementById('curso').value;
-        const notaFinal = document.getElementById('notaFinal').value;
+        const notaFinal = parseFloat(document.getElementById('notaFinal').value);
 
+        if (notaFinal < 0 || notaFinal > 10) {
+            alert(`Nota final inválida.Digite um valor entre 0 e 10`);
+            return;
+        }
 
-        const aluno = new Aluno(nome, idade, curso, notaFinal);
+        if (!nome || isNaN(idade) || !curso || isNaN(notaFinal)) {
+            alert(`Preencha os campos corretamente!`);
+            return;
+        }
 
-        this.alunos.push(aluno); //pega as info e imprime na tabela 
-        this.atualizarTabela(); // atualiza a tabela com nova lista de alunos
-        this.limparFormulario(); // limpa os campos de formulário
+        if (nome || idade || curso || notaFinal) {
+            const confirmar = confirm('Deseja salvar esse aluno(a)?');
+
+            if (confirmar) {
+                console.log(`Aluno(a) salvado com sucesso!`);
+
+                const aluno = new Aluno(nome, idade, curso, notaFinal);
+
+                this.alunos.push(aluno); //pega as info e imprime na tabela 
+                this.atualizarTabela(); // atualiza a tabela com nova lista de alunos
+                this.limparFormulario(); // limpa os campos de formulário
+
+                console.log(aluno.toString()); 
+            }
+
+            else {
+                console.log(`Ação cancelada.`);
+                return;
+            }
+        }
+
     }
 
-    editarAluno(event){
+    editarAluno(event) {
         const row = event.target.closest('tr'); //obtém a linha da tabela onde o botão foi clicado
         const index = row.rowIndex - 1;
-        const aluno = this. alunos[index];
+        const aluno = this.alunos[index];
 
         document.getElementById('nome').value = aluno.nome;
         document.getElementById('idade').value = aluno.idade;
         document.getElementById('curso').value = aluno.curso;
         document.getElementById('notaFinal').value = aluno.notaFinal;
 
-        
-        this.alunos.splice(index, 1); //remove aluno da lista
-        this.atualizarTabela(); // atualiza a tabela com a nova lista de alunos
+        if (nome || idade || curso || notaFinal) {
+            const confirmar = confirm('Deseja editar esse aluno(a)?');
+
+            if (confirmar) {
+                console.log(`Aluno(a) editado com sucesso!`);
+
+                this.alunos.splice(index, 1); //remove aluno da lista
+                this.atualizarTabela(); // atualiza a tabela com a nova lista de alunos
+            }
+
+            else {
+                console.log(`Ação cancelada.`);
+                return;
+            }
+        }
     }
+
 
     excluirAluno(e) {
-        e.preventDefault();
-        const row = e.target.closest('tr'); 
-        const index = row.rowIndex - 1;
 
-        this.alunos.splice(index, 1);
-        this.atualizarTabela();
+        const confirmar = confirm(`Deseja excluir esse aluno(a) ? `);
 
+        if (confirmar) {
+            console.log(`Aluno(a) excluído com sucesso!`);
+
+            e.preventDefault();
+            const row = e.target.closest('tr');
+            const index = row.rowIndex - 1;
+
+            this.alunos.splice(index, 1);
+            this.atualizarTabela();
+
+        }
+
+        else {
+            console.log(`Ação cancelada.`);
+            return;
+        }
     }
 
-    atualizarTabela(){
+    atualizarTabela() {
         const tabela = document.getElementById('tabela');
         tabela.innerHTML = '';
         this.alunos.forEach((aluno, index) => { //repete a linha de cada aluno no seu indice 
@@ -75,6 +147,15 @@ class AlunoController {
             row.insertCell(2).innerText = aluno.idade;
             row.insertCell(3).innerText = aluno.curso;
             row.insertCell(4).innerText = aluno.notaFinal;
+            const status =  row.insertCell(5);
+            
+            if(aluno.isAprovado()) {
+                status.innerText = "APROVADO!";
+            }
+
+            else {
+                status.innerText = "REPROVADO"; 
+            }
 
             const btnEditar = document.createElement('button');
             btnEditar.innerText = 'Editar';
@@ -85,10 +166,10 @@ class AlunoController {
             btnExcluir.type = 'button'
 
             //cria uma célula para os botões 
-            const actionCell = row.insertCell(5);
+            const actionCell = row.insertCell(6);
 
             //adiciona o botão de editar e excluir na célula
-            actionCell.appendChild(btnEditar); 
+            actionCell.appendChild(btnEditar);
             actionCell.appendChild(btnExcluir);
             actionCell.style.display = 'flex';
             actionCell.style.gap = '5px';
@@ -96,18 +177,18 @@ class AlunoController {
 
             btnEditar.addEventListener('click', (event) => this.editarAluno(event));
             btnExcluir.addEventListener('click', (event) => this.excluirAluno(event));
-        
+
         });
 
     }
 
-    limparFormulario(){
+    limparFormulario() {
         document.getElementById('nome').value = '';
         document.getElementById('idade').value = '';
         document.getElementById('curso').value = '';
         document.getElementById('notaFinal').value = '';
-    
-    
+
+
     }
 }
 
